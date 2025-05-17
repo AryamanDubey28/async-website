cf259b6f92408b0f9ff09c8d6986839ca64f54e9
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2025-05-17 16:00:17 +0100

    remove speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -92,7 +91,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
-        <SpeedInsights />
+     
       </body>
     </html>
   );
